d98b9c0f5a53054df0ac4c1f299e92e5c1b257ad
Nitin Goyal <nitingoyal2707@gmail.com>
2022-06-20 15:48:28 +0530

    Committed.

--- src/javaOOPS/MainClass.java
+++ src/javaOOPS/MainClass.java
@@ -1,19 +1,19 @@
 package javaOOPS;
 
-import java.util.Scanner;
-
 public class MainClass {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
+//        Scanner scn = new Scanner(System.in);
 
-        Animal a1 = new Animal();
-        System.out.print("Enter the age of your dog: ");
-        a1.age = scn.nextInt();
-        scn.nextLine();
-        System.out.print("Enter the name of your dog: ");
-        a1.name = scn.nextLine();
+        Animal a1 = new Animal("Tommy", 5);
+//        System.out.print("Enter the age of your dog: ");
+//        a1.age = scn.nextInt();
+//        scn.nextLine();
+//        System.out.print("Enter the name of your dog: ");
+//        a1.name = scn.nextLine();
+        Animal a2 = new Animal("Jack", 7);
         a1.bark();
         a1.play();
+        a2.display();
     }
 
 }
@@ -21,6 +21,15 @@ public class MainClass {
 class Animal {
     String name;
     int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println(name + " " + age);
+    }
 
     void play() {
         System.out.println(name + " is playing.");
